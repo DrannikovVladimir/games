4b839183f07b95410e4fe878fbc9924d38c297ce
Vladimir Drannikov <v.drannikov@alef.im>
2022-07-27 16:36:12 +0600

    Edited files project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { useDispatch } from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom'; 
 
-function App() {
+import { setPlatforms } from './store/slices/gameSlice';
+import API, { token } from './service';
+
+import MainPage from './pages/MainPage/MainPage';
+import GamePage from './pages/GamePage/GamePage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+
+const App = () => {
+  const dispatch = useDispatch(); 
+
+  React.useEffect(() => {
+    const fetchPlatforms = async () => {
+      try {
+        const { data } = await API.get('/platforms/lists/parents', { params: { ...token } });
+        // console.log(data);
+        dispatch(setPlatforms({ platforms: data.results }));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchPlatforms();
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+        <Route path='/game' element={<GamePage />} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>
+    </Router>
   );
 }
 
